8cf3f443c2d861c7b7786405cd48447bbc7ba760
Koveto <90483981+Koveto@users.noreply.github.com>
2024-10-29 00:34:05 -0500

    Keypad input detection

--- button_input.py
+++ button_input.py
@@ -5,6 +5,10 @@ button_input.py
 Using edge interrupts, toggle 4 LEDs
 depending on which button is pressed.
 Handles debouncing by waiting 100 ms.
+
+GP16-19 -> K1-4 
+GP20-22 -> LED1-3
+GP26 -> LED4
 """
 
 from machine import Pin
@@ -17,7 +21,6 @@ led4 = Pin(26, Pin.OUT)
 
 button1 = Pin(19, Pin.IN, Pin.PULL_UP)
 def button1Pressed(button1):
-    global flag
     sleep(0.1)
     if (button1.value() == 0):
         led1.toggle()
@@ -25,7 +28,6 @@ button1.irq(trigger=Pin.IRQ_FALLING, handler=button1Pressed)
 
 button2 = Pin(18, Pin.IN)
 def button2Pressed(button2):
-    global flag
     sleep(0.1)
     if (button2.value() == 0):
         led2.toggle()
@@ -33,7 +35,6 @@ button2.irq(trigger=Pin.IRQ_FALLING, handler=button2Pressed)
 
 button3 = Pin(17, Pin.IN)
 def button3Pressed(button3):
-    global flag
     sleep(0.1)
     if (button3.value() == 0):
         led3.toggle()
@@ -41,14 +42,12 @@ button3.irq(trigger=Pin.IRQ_FALLING, handler=button3Pressed)
 
 button4 = Pin(16, Pin.IN)
 def button4Pressed(button4):
-    global flag
     sleep(0.1)
     if (button4.value() == 0):
         led4.toggle()
 button4.irq(trigger=Pin.IRQ_FALLING, handler=button4Pressed)
 
 def main():
-    global flag
     while(True):
         pass
         
